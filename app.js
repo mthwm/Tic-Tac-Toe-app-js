aaa9b1a78b60fec306ccbfbafa813d2424f8a7e1
mthwm <matthewm1@seznam.cz>
2022-01-10 23:44:55 +0100

    .

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 var x = true;
 
 let main = (divId) => {
-  let currentDiv = document.getElementById(divId);
   fetch("https://api.countapi.xyz/update/Super_Stranka/ZULUL/?amount=1");
 
-  switch (x) {
-    case true:
-      currentDiv.innerHTML = '<i class="fas fa-times"></i>';
-      x = false;
-      break;
-    default:
-      currentDiv.innerHTML = '<i class="far fa-circle"></i>';
-      x = true;
-      break;
+  let currentDiv = document.getElementById(divId);
+  let currentDivClassList = currentDiv.classList;
+  console.log(currentDivClassList);
+
+  if (currentDivClassList[1] == "checked") {
+    console.log("you can't rewrite");
+  } else {
+    switch (x) {
+      case true:
+        currentDiv.innerHTML = '<i class="fas fa-times"></i>';
+        currentDiv.classList += " checked";
+        x = false;
+        break;
+      default:
+        currentDiv.innerHTML = '<i class="far fa-circle"></i>';
+        currentDiv.classList += " checked";
+        x = true;
+        break;
+    }
   }
 };
