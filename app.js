25db143d7fe1ee7c54a6e29e3d73e0bcee909d3f
mthwm <matthewm1@seznam.cz>
2022-01-10 23:22:53 +0100

    .

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+var x = true;
+
 let main = (divId) => {
-  console.log(divId);
+  let currentDiv = document.getElementById(divId);
+  fetch("https://api.countapi.xyz/update/Super_Stranka/ZULUL/?amount=1");
+
+  switch (x) {
+    case true:
+      currentDiv.innerHTML = '<i class="fas fa-times"></i>';
+      x = false;
+      break;
+    default:
+      currentDiv.innerHTML = '<i class="far fa-circle"></i>';
+      x = true;
+      break;
+  }
 };
